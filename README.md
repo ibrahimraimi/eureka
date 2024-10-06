cb92eeef2934a3623ab7eacec1c5e9dc039f7271
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-10-06 16:55:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Eureka: A Go-Powered Search Engine
+
+Eureka is a search engine built in Go, designed for fast indexing, efficient search, specialized content.
+
+## Features
+
+* **Web Crawling:**  Crawls web pages to gather data, respecting `robots.txt` rules.
+* **Indexing:**  Creates an inverted index to efficiently store and retrieve information.
+* **Search:**  Allows users to search for information using keywords.
+* **Ranking:**  Ranks search results based on relevance
+
+## Getting Started
+
+1. **Prerequisites:** Make sure you have Go installed on your system.
+2. **Clone the repository:** `git clone https://github.com/ibrahimraimi/eureka.git`
+3. **Build:**  `go build`
+4. **Run:**  `./eureka`
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	env := godotenv.Load()
+
+	if env != nil {
+		panic("Cannot find environment variables")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":5000"
+	} else {
+		port = ":" + port
+	}
+
+	app := fiber.New(fiber.Config{
+		IdleTimeout: 5 * time.Second,
+	})
+
+	app.Use(compress.New())
+
+	// start server and listen for shutdown cmd
+	go func() {
+		if err := app.Listen(port); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	<-c // block the main thread until interupted
+	app.Shutdown()
+	fmt.Println("Shutting down server")
+}
